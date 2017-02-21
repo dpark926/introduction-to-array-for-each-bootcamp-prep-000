4091f4d43524b3184f017fa55a3214ac954efd41
Daniel Park <dpeace926@gmail.com>
2017-02-21 20:28:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeCompletely(element, index, array) {
+
+
+}
+
+function doToElementsInArray(array, callback) {
+  // array.forEach(ele, idx, array) {
+  //   callback(array)
+  //
+  // }
+  callback(array)
+}
